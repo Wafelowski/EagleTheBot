73e8525409ae99d16538b76eb78d6bebafff218e
Wafel <heavywolfpl@gmail.com>
2021-07-05 20:58:41 +0200

    still not finished

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var bot = {};
 //-=-=-=-=-=-=-=-
 //Load cogs
 //-=-=-=-=-=-=-=-
-const coreCogs = ["./cogs/test.js", "./cogs/moderation/purge.js", "./cogs/moderation/kick.js", "./cogs/moderation/bans.js"]
+const coreCogs = ["./cogs/setup.js", "./cogs/useless/test.js", "./cogs/moderation/purge.js", "./cogs/moderation/kick.js", "./cogs/moderation/bans.js"]
 var loadedCogs = {};
 var listeners = {};
 
@@ -42,6 +42,14 @@ client.on('ready', () => {
 
   bot.client.user.setStatus('online');
   bot.client.user.setActivity('przejazdy alarmowe', { type: 'WATCHING' });
+
+  let serversDB = new sqlite.Database('./db/servers.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+  serversDB.run(`CREATE TABLE IF NOT EXISTS servers(serverid INTEGER NOT NULL, serverowner TEXT NOT NULL)`);
+  let blacklistDB = new sqlite.Database('./db/blacklist.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+  blacklistDB.run(`CREATE TABLE IF NOT EXISTS users(userid INTEGER NOT NULL, usertag TEXT NOT NULL, staff TEXT NOT NULL)`);
+  let botStaffDB = new sqlite.Database('./db/botStaff.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+  botStaffDB.run(`CREATE TABLE IF NOT EXISTS staff(userid INTEGER NOT NULL, usertag TEXT NOT NULL, nickname TEXT NOT NULL)`);
+
   bot.ready = true;
 });
 
@@ -73,6 +81,63 @@ client.on('message', msg => {
     } else {
       msg.reply("Komenda nieznana.");
     }
+});
+
+client.on('guildMemberUpdate', (oldMember, newMember) => {
+  if (!bot.ready) return;
+  const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+	if (!addedRoles.size > 0) return;
+  //const channel = bot.client.channels.cache.get('853208523605147659');
+  var guildID = oldMember.guild.id;
+  console.log("line 92")
+  var serverDB = new sqlite.Database(`./db/servers/${guildID}.db`, sqlite.OPEN_READWRITE);
+  serverDB.all('SELECT * FROM roles WHERE role = ?', "verification", function(err, rows) {
+    console.log("line 95")
+    if (err) {
+      console.log(`guildMemberUpdate Error: ${err}`);
+      return false;
+    }
+    if (rows === undefined) {
+      return false;
+    } 
+    else {
+      let VerificationRole = [];
+      rows.forEach( row => VerificationRole.push(`${row.roleid}`) );
+      console.log("line 105");
+      console.log(VerificationRole);
+      if (oldMember.roles.cache.some(role => role.id === VerificationRole)) {
+        const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+        console.log("line 114")
+        var addedRoleID = addedRoles.map(r => r.id)
+        var addedRoleName = addedRoles.map(r => r.name)
+        console.log(`${addedRoleID.values().next().value}`)
+        if (addedRoles.size > 0) console.log(`Role "${addedRoles.map(r => r.name)}" zostały przyznane ${oldMember.displayName}.`);
+        newMember.roles.remove(addedRoleID, "Anty-Alt")
+        if (addedRoles.size > 0) console.log(`Rola "${addedRoleName}" zostały usunięte ${oldMember.displayName}.`);
+      }
+      serverDB.close();
+      return VerificationRole;
+    }
+  });
+
+  if (oldMember.roles.cache.some(role => role.id === VerificationRole)) {
+    const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+    console.log("line 114")
+    var addedRoleID = addedRoles.map(r => r.id)
+    var addedRoleName = addedRoles.map(r => r.name)
+    console.log(`${addedRoleID.values().next().value}`)
+    if (addedRoles.size > 0) console.log(`Role "${addedRoles.map(r => r.name)}" zostały przyznane ${oldMember.displayName}.`);
+    newMember.roles.remove(addedRoleID, "Anty-Alt")
+    if (addedRoles.size > 0) console.log(`Rola "${addedRoleName}" zostały usunięte ${oldMember.displayName}.`);
+  }
+  
+  // // If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+	// const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+	// if (removedRoles.size > 0) console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
+	// // If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
+	// const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+	// if (addedRoles.size > 0) console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+
 });
 
 bot.registerCommand = function (command, func) {
---
+++ cogs/setup.js
@@ -0,0 +1,148 @@
+const Discord = require('discord.js');
+const config = require('../config.json');
+const sqlite = require('sqlite3');
+const { promisify } = require("util");
+var bot = {};
+
+var setupcmd = function (msg) {
+    if (!msg.member.permissions.has("ADMINISTRATOR") || msg.member.id != config.ownerID) {
+        msg.reply("Brak uprawnień!");
+        return
+    }
+    const args = msg.content.trim().split(' ');
+    try {
+        var guildID = msg.guild.id
+        var guildOwner = msg.guild.ownerID
+        let serverDB = new sqlite.Database(`./db/servers/${guildID}.db`, sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
+        serverDB.run(`CREATE TABLE IF NOT EXISTS server(serverid INTEGER NOT NULL, serverowner TEXT NOT NULL)`);
+        serverDB.run(`CREATE TABLE IF NOT EXISTS roles(role TEXT NOT NULL, roleid TEXT NOT NULL)`);
+        let insertdata = serverDB.prepare(`INSERT INTO server VALUES(?,?)`);
+        insertdata.run(guildID, guildOwner);
+        insertdata.finalize();
+        serverDB.close();
+    }
+    catch (error) {
+        msg.reply(`Error \n\`${error}\``)
+    }
+    let serverDB = new sqlite.Database(`./db/servers/${guildID}.db`, sqlite.OPEN_READWRITE);
+    if (args[0] == "info") {
+        var modsID = [];
+        var adminsID = [];
+        serverDB.all('SELECT * FROM roles WHERE role = ?',"mod", function(err, rows) {
+            if (err) {
+                modsID = "Wystąpił błąd - Moderator"
+                console.log(err);
+                return modsID;
+            }
+            if (rows === undefined) {
+                modsID = "Brak: Moderator"
+                return modsID;
+            } 
+            else {
+                rows.forEach( row => modsID.push(`<@&${row.roleid}>`) )
+                //msg.reply(`modsID - ${modsID.join(', ')}`)
+            }
+        });
+        serverDB.all('SELECT * FROM roles WHERE role = ?',"admin", function(err, rows) {
+            if (err) {
+                adminsID = "Wystąpił błąd - Administrator"
+                console.log(err);
+                return adminsID;
+            }
+            if (rows === undefined) {
+                adminsID = "Brak"
+                return adminsID;
+            } 
+            else {
+                rows.forEach( row => adminsID.push(`<@&${row.roleid}>`) )
+                //msg.reply(`modsID - ${adminsID.join(', ')}`)
+            }
+        });
+        // let query2 = `SELECT * FROM roles WHERE role = ?`;
+        // serverDB.get(query2, "admin", (err, row) => {
+        //     if (err) {
+        //         adminID = "Wystąpił błąd - Administrator"
+        //         console.log(err);
+        //         return;
+        //     }
+        //     if (row === undefined) {
+        //         adminID = "Brak: Administrator"
+        //         return;
+        //     } else {
+        //         adminID = row.roleid;
+        //         //msg.reply(`modsID - ${modID}`)
+        //     }
+        // });
+        serverDB.close()
+        // const embed = new Discord.MessageEmbed()
+        //     .setAuthor(`Setup - ${msg.guild.name}`)
+        //     .setDescription(`Administratorzy - ${adminsID.join(', ')}. \nModeratorzy - ${mods.join(', ')}.`)
+        //     .setColor("#ff0000")
+        //     .setFooter(config.footerCopyright, config.footerCopyrightImage)
+        //     .setTimestamp()
+        // msg.reply( {embeds: [embed] })
+    }
+    if (args[0] == "add") {
+        if ((args[1] == "admin")|| args[1] == "administrator") {
+            let insertdata = serverDB.prepare(`INSERT INTO roles VALUES(?,?)`);
+            insertdata.run("admin", args[2]);
+            insertdata.finalize();
+            serverDB.close();
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(`Setup - ${msg.guild.name}`)
+            .setDescription(`Dodano rolę <@&${args[2]}> jako Administrator.`)
+            .setColor("#ff0000")
+            .setFooter(config.footerCopyright, config.footerCopyrightImage)
+            .setTimestamp()
+            msg.reply( {embeds: [embed] })
+        }
+        if ((args[1] == "mod") || (args[1] == "moderator")) {
+            let insertdata = serverDB.prepare(`INSERT INTO roles VALUES(?,?)`);
+            insertdata.run("mod", args[2]);
+            insertdata.finalize();
+            serverDB.close();
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(`Setup - ${msg.guild.name}`)
+            .setDescription(`Dodano rolę <@&${args[2]}> jako Moderator.`)
+            .setColor("#ff0000")
+            .setFooter(config.footerCopyright, config.footerCopyrightImage)
+            .setTimestamp()
+            msg.reply( {embeds: [embed] })
+        }
+        if ((args[1] == "weryfikacja") || args[1] == "verification") {
+            let insertdata = serverDB.prepare(`INSERT INTO roles VALUES(?,?)`);
+            insertdata.run("verification", args[2]);
+            insertdata.finalize();
+            serverDB.close();
+            const embed = new Discord.MessageEmbed()
+            .setAuthor(`Setup - ${msg.guild.name}`)
+            .setDescription(`Dodano rolę <@&${args[2]}> jako rolę weryfikacji w toku.`)
+            .setColor("#ff0000")
+            .setFooter(config.footerCopyright, config.footerCopyrightImage)
+            .setTimestamp()
+            msg.reply( {embeds: [embed] })
+        }
+    }
+    if (args[0] == "remove") {
+        let removedata = serverDB.prepare(`DELETE FROM roles WHERE roleid = ${args[1]}`);
+        removedata.run();
+        removedata.finalize();
+        serverDB.close();
+        const embed = new Discord.MessageEmbed()
+        .setAuthor(`Setup - ${msg.guild.name}`)
+        .setDescription(`Usunięto rolę <@&${args[1]}> z listy.`)
+        .setColor("#ff0000")
+        .setFooter(config.footerCopyright, config.footerCopyrightImage)
+        .setTimestamp()
+        msg.reply( {embeds: [embed] })
+    }
+    msg.react('✅')
+}
+
+var setup = function (b) {
+    bot = b;
+    bot.registerCommand("setup", setupcmd);
+}
+
+exports.requires = [];
+exports.setup = setup;
