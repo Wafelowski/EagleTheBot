79ee4863bf6106357d26515d26f5bb41dfe21b4f
Wafel <heavywolfpl@gmail.com>
2021-07-06 13:35:46 +0200

    info still not working, rest is fine

--- cogs/setup.js
+++ cogs/setup.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
 const sqlite = require('sqlite3');
-const { promisify } = require("util");
+//const { promisify } = require("util");
 var bot = {};
 
 var setupcmd = function (msg) {
@@ -16,9 +16,30 @@ var setupcmd = function (msg) {
         let serverDB = new sqlite.Database(`./db/servers/${guildID}.db`, sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
         serverDB.run(`CREATE TABLE IF NOT EXISTS server(serverid INTEGER NOT NULL, serverowner TEXT NOT NULL)`);
         serverDB.run(`CREATE TABLE IF NOT EXISTS roles(role TEXT NOT NULL, roleid TEXT NOT NULL)`);
-        let insertdata = serverDB.prepare(`INSERT INTO server VALUES(?,?)`);
-        insertdata.run(guildID, guildOwner);
-        insertdata.finalize();
+        serverDB.all('SELECT * FROM server', function(err, rows) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (rows === undefined) {
+                let insertdata = serverDB.prepare(`INSERT INTO server VALUES(?,?)`);
+                insertdata.run(guildID, guildOwner);
+                insertdata.finalize();
+                return;
+            } 
+            else {
+                var serverowner = [];
+                rows.forEach( row => serverowner.push(`${row.serverowner}`) );
+                if (serverowner != guildOwner) {
+                    let replacedata = serverDB.prepare(`REPLACE INTO server VALUES(?,?)`);
+                    replacedata.run(guildID, guildOwner);
+                    replacedata.finalize();
+                }
+            }
+        });
+        //let insertdata = serverDB.prepare(`INSERT INTO server VALUES(?,?)`);
+        //insertdata.run(guildID, guildOwner);
+        //insertdata.finalize();
         serverDB.close();
     }
     catch (error) {
@@ -28,6 +49,12 @@ var setupcmd = function (msg) {
     if (args[0] == "info") {
         var modsID = [];
         var adminsID = [];
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(`Setup - ${msg.guild.name}`)
+            //.setDescription(`Administratorzy - ${adminsID.join(', ')}. \nModeratorzy - ${modsID.join(', ')}.`)
+            .setColor("#ff0000")
+            .setFooter(config.footerCopyright, config.footerCopyrightImage)
+            .setTimestamp()
         serverDB.all('SELECT * FROM roles WHERE role = ?',"mod", function(err, rows) {
             if (err) {
                 modsID = "Wystąpił błąd - Moderator"
@@ -39,9 +66,18 @@ var setupcmd = function (msg) {
                 return modsID;
             } 
             else {
-                rows.forEach( row => modsID.push(`<@&${row.roleid}>`) )
+                rows.forEach( row => modsID.push(`<@&${row.roleid}>`) );
+                console.log("line 49");
+                embed.addFields(
+                    { name: "Moderatorzy", value: `${modsID.join(', ')}`, inline: false },
+                    { name: "Moderatorzy", value: `${modsID.join(', ')}`, inline: false },
+                );
                 //msg.reply(`modsID - ${modsID.join(', ')}`)
             }
+            embed.addFields(
+                { name: "Moderatorzy", value: `${modsID.join(', ')}`, inline: false },
+                { name: "Moderatorzy", value: `${modsID.join(', ')}`, inline: false },
+            );
         });
         serverDB.all('SELECT * FROM roles WHERE role = ?',"admin", function(err, rows) {
             if (err) {
@@ -54,33 +90,20 @@ var setupcmd = function (msg) {
                 return adminsID;
             } 
             else {
-                rows.forEach( row => adminsID.push(`<@&${row.roleid}>`) )
-                //msg.reply(`modsID - ${adminsID.join(', ')}`)
+                rows.forEach( row => adminsID.push(`<@&${row.roleid}>`) );
+                console.log("line 67");
+                embed.addFields(
+                    { name: "Administratorzy", value: `${adminsID.join(', ')}`, inline: false },
+                    { name: "Moderatorzy", value: `${modsID.join(', ')}`, inline: false },
+                );
             }
+            embed.addFields(
+                { name: "Administratorzy", value: `${adminsID.join(', ')}`, inline: false },
+                { name: "Moderatorzy", value: `${modsID.join(', ')}`, inline: false },
+            );
         });
-        // let query2 = `SELECT * FROM roles WHERE role = ?`;
-        // serverDB.get(query2, "admin", (err, row) => {
-        //     if (err) {
-        //         adminID = "Wystąpił błąd - Administrator"
-        //         console.log(err);
-        //         return;
-        //     }
-        //     if (row === undefined) {
-        //         adminID = "Brak: Administrator"
-        //         return;
-        //     } else {
-        //         adminID = row.roleid;
-        //         //msg.reply(`modsID - ${modID}`)
-        //     }
-        // });
         serverDB.close()
-        // const embed = new Discord.MessageEmbed()
-        //     .setAuthor(`Setup - ${msg.guild.name}`)
-        //     .setDescription(`Administratorzy - ${adminsID.join(', ')}. \nModeratorzy - ${mods.join(', ')}.`)
-        //     .setColor("#ff0000")
-        //     .setFooter(config.footerCopyright, config.footerCopyrightImage)
-        //     .setTimestamp()
-        // msg.reply( {embeds: [embed] })
+        msg.reply( {embeds: [embed] })
     }
     if (args[0] == "add") {
         if ((args[1] == "admin")|| args[1] == "administrator") {
