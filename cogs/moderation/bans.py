d2a76da67bd3573f44cb5b06f4ebb4c5b52a79c4
Wafelowski <heavywolfpl@gmail.com>
2021-11-16 19:11:29 +0100

    improved commands

--- cogs/moderation/bans.py
+++ cogs/moderation/bans.py
@@ -2,11 +2,12 @@ import json
 import discord
 from discord.ext import commands
 from discord.ext.commands.core import has_permissions
-from discord.role import R
 
 with open("config.json", "r") as config: 
     data = json.load(config)
     prefix = data["prefix"]
+    footer = data["footerCopyright"]
+    footer_img = data["footerCopyrightImage"]
 
 class Bans(commands.Cog):
     def __init__(self, bot):
@@ -16,17 +17,51 @@ class Bans(commands.Cog):
 
     @bot.command()
     @has_permissions(ban_members=True)  
-    async def ban(ctx, member : discord.Member, *, reason = None):
-        await member.ban(reason = reason)
+    async def ban(self, ctx, member : discord.Member, *, reason = None):
+        if member.id == ctx.author.id:
+            await ctx.send("Dlaczego chcesz zbanować samego siebie?")
+            return
+        days=0
+        daysMsg = ""
+        if "-24h" in ctx.message.content:
+            days=1
+            reason = reason.replace('-24h', '')
+            daysMsg = "\nDodatkowo usunięto wiadomości z ostatnich 24 godzin."
+        if "-7d" in ctx.message.content:
+            days=7
+            reason = reason.replace('-7d', '')
+            daysMsg = "\nDodatkowo usunięto wiadomości z ostatnich 7 dni."
+        silentMsg = "[Flaga -s]"
+        if not "-s" in ctx.message.content:
+            silentMsg = ""
+            description = f"""Otrzymano banicję. Powód: {reason}"""
+            embed=discord.Embed(description=description, color=0xff0000, timestamp=ctx.message.created_at)
+            embed.set_author(name=ctx.guild.name)
+            embed.set_footer(text=footer, icon_url=footer_img)
+            await member.send(embed=embed)
+        reason = reason.replace('-s', '')
+        description = f"""**Nadano banicję.**\n
+        **Użytkownik**: <@{member.id}> ({member.name}#{member.discriminator}) 
+        **Administrator**: <@{ctx.author.id}> ({ctx.author.id}) 
+        **Powód**: {reason} {silentMsg} \n{daysMsg}"""
+        embed2=discord.Embed(description=description, color=0xff0000, timestamp=ctx.message.created_at)
+        embed2.set_author(name=ctx.guild.name)
+        embed2.set_footer(text=footer, icon_url=footer_img)
+        try:
+            await member.ban(days=days, reason = f"{reason} {silentMsg} {daysMsg}")
+        except:
+            await ctx.send("Wystąpił błąd. [Bans-53]")
+        await ctx.send(embed=embed2)
+        
 
     @ban.error
     async def ban_error(self, ctx, error):
         if isinstance(error, commands.errors.CommandInvokeError):
             error = error.original
-        elif isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send("Nie podano wszystkich argumentów!")
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.reply("Nie podano wszystkich argumentów!", mention_author=False)
         elif isinstance(error, commands.errors.MissingPermissions):
-            await ctx.send("Brak uprawnień!")
+            await ctx.reply("Brak uprawnień!", mention_author=False)
             raise error
         else:
             await ctx.send("Nieznany błąd!")
--- cogs/moderation/kicks.py
+++ cogs/moderation/kicks.py
@@ -7,6 +7,8 @@ from discord.ext.commands.errors import MissingPermissions
 with open("config.json", "r") as config: 
     data = json.load(config)
     prefix = data["prefix"]
+    footer = data["footerCopyright"]
+    footer_img = data["footerCopyrightImage"]
 
 class Kicks(commands.Cog):
     def __init__(self, bot):
@@ -16,17 +18,40 @@ class Kicks(commands.Cog):
 
     @bot.command()
     @has_permissions(kick_members=True)  
-    async def kick(ctx, member : discord.Member, *, reason = None):
-        await member.kick(reason = reason)
+    async def kick(self, ctx, member : discord.Member, *, reason = None):
+        if member.id == ctx.author.id:
+            await ctx.send("Dlaczego chcesz wyrzucić samego siebie?")
+            return
+        silentMsg = "[Flaga -s]"
+        if not "-s" in ctx.message.content:
+            silentMsg = ""
+            description = f"""Otrzymano kopa w dupe. Powód: {reason}"""
+            embed=discord.Embed(description=description, color=0xff0000, timestamp=ctx.message.created_at)
+            embed.set_author(name=ctx.guild.name)
+            embed.set_footer(text=footer, icon_url=footer_img)
+            await member.send(embed=embed)
+        reason = reason.replace('-s', '')
+        description = f"""**Wyrzucono użytkownika.**\n
+        **Użytkownik**: <@{member.id}> ({member.name}#{member.discriminator}) 
+        **Administrator**: <@{ctx.author.id}> ({ctx.author.id}) 
+        **Powód**: {reason} {silentMsg}"""
+        embed2=discord.Embed(description=description, color=0xff0000, timestamp=ctx.message.created_at)
+        embed2.set_author(name=ctx.guild.name)
+        embed2.set_footer(text=footer, icon_url=footer_img)
+        try:
+            await member.kick(reason = f"{reason} {silentMsg}")
+        except:
+            await ctx.send("Wystąpił błąd. [Kicks-44]")
+        await ctx.send(embed=embed2)
 
     @kick.error
     async def kick_error(self, ctx, error):
         if isinstance(error, commands.errors.CommandInvokeError):
             error = error.original
-        elif isinstance(error, commands.errors.MissingRequiredArgument):
-            await ctx.send("Nie podano wszystkich argumentów!")
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.reply("Nie podano wszystkich argumentów!", mention_author=False)
         elif isinstance(error, commands.errors.MissingPermissions):
-            await ctx.send("Brak uprawnień!")
+            await ctx.reply("Brak uprawnień!", mention_author=False)
             raise error
         else:
             await ctx.send("Nieznany błąd!")
